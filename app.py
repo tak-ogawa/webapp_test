f3e46f0c0d85dd4c49e405ffe447815d0632fd81
Takanobu (Tak) Ogawa <140125779+tak-ogawa@users.noreply.github.com>
2024-10-15 16:10:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.write("hello streamlit")
+enable = st.checkbox("Enable camera")
+picture = st.camera_input("Take a picture", disabled=not enable)
+
+if picture:
+    st.image(picture)
